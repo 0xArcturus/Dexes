14a96c3651f8414bff6e0585ac80fbdaf60aead2
Ch0ckblain <rouraarturo@protonmail.com>
2023-03-05 13:57:27 +0100

    security implementations

--- contracts/DexV1.sol
+++ contracts/DexV1.sol
@@ -12,76 +12,125 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 //Tokens to ETH requires the Dex to be approved to take ERC20 tokens
 //It controls the liquidity provided to this contract by calling mint and burn on a ERC20 contract that adds or substracts these tokens from the providers balance.
 contract DexV1 {
-    //SafeMath isn't necessary
+
+
     IERC20 token;
     uint256 public totalLiquidity;
     address public LPTokenAddress;
+    bool internal LPTokenAddressSet = false;
+    bool internal initialized = false;
     address public LPTokenAddressSetter;
-    event deposited(uint256 indexed _ethAmount, uint256 indexed amountSentToFunc);
-    event withdrawed(uint256 indexed _ethAmount, uint256 indexed amountSentToFunc);
-    event ethToTokenExchanged(
-        address indexed user,
-        uint256 indexed _ethAmount,
-        uint256 indexed time
+
+    event Initialized(
+        uint256 indexed ETHAmount,
+        uint256 indexed TokenAmount,
+        address indexed initializer
     );
-    event tokenToEthExchanged(
-        address indexed user,
-        uint256 indexed _ethAmount,
-        uint256 indexed time
+    event Deposited(
+        address indexed user, 
+        uint256 indexed ethDeposited, 
+        uint256 indexed tokensDeposited
     );
-    event LPtokenAddressSet(address indexed LPAddress, address indexed user, uint256 indexed time);
+    event Withdrawed(
+        address indexed user, 
+        uint256 indexed ethWithdrawed, 
+        uint256 indexed tokensWithdrawed
+    );
+    event EthToTokenExchanged(
+        address indexed user,
+        uint256 indexed ethSent,
+        uint256 indexed tokensBought
+    );
+    event TokenToEthExchanged(
+        address indexed user,
+        uint256 indexed tokensSent,
+        uint256 indexed ethBought
+    );
+    event LPTokenAddressSet(
+        address indexed LPTokenAddress, 
+        address indexed user, 
+        uint256 indexed time 
+    );
+
+    /// @notice Checks in several functions that the contract has been initialized.
+    modifier initialized {
+        require(initialized == true, "liquidity pool has not been initialized yet");
+         _;
+    } 
 
     constructor(address yeahTokenAddress) public {
         token = IERC20(yeahTokenAddress);
         LPTokenAddressSetter = msg.sender;
     }
 
+    /// @notice Function called by the deployer of the contract that sets the address for the LP tokens. It should be set only once.
+    /// @dev The LP token address represents a huge weak point, the LPTokenAddressSetter address is a fully trusted account.
+    /// @param _LPTokenAddress The address of the ERC20 LP token, that represents the liquidity providers stake in the pool.
     function LPTokenSetAddress(address _LPTokenAddress) public {
+        require(!LPTokenAddressSet, "LP token address has already been set")
         require(msg.sender == LPTokenAddressSetter, "you are not the address setter");
         LPTokenAddress = _LPTokenAddress;
+        LPTokenAddressSet = true;
+        emit LPTokenAddressSet(_LPTokenAddress, msg.sender, block.timestamp);
     }
 
-    //to init, the contract must be approved to perform the transfer
+    /// @notice Initializes the pool with reserves of DEX token and ETH
+    /// @dev The initial ratio deposited will hugely impact exchange price of assets
+    /// @param tokens The amount of DEX tokens the user wants to deposit in the liquidity pool with the msg.value
+    /// @return The total amount of liquidity, that's represented by liquidity tokens.
     function init(uint256 tokens) public payable returns (uint256) {
+        require(LPTokenAddressSet == true, "LP token Address has not been set yet");
         require(totalLiquidity == 0, "dex has already been initialized");
         // if someone send eth before calling the init function, the liquidity provided will be captured by
         //the user that calls init.
         totalLiquidity = address(this).balance;
+        
+       
+        require(token.transferFrom(msg.sender, address(this), tokens)); 
+
         //mint the LP tokens to the address that initializes the pool
         (bool success, ) = LPTokenAddress.call(
             abi.encodeWithSignature("mintTokensTo(address,uint256)", msg.sender, totalLiquidity)
         );
         require(success, "mint tx failed");
-        require(token.transferFrom(msg.sender, address(this), tokens)); //transfer call for the amount we set as an input to this func
+        initialized = true;
+        emit Initialized(totalLiquidity, tokens, msg.sender);
         return totalLiquidity;
     }
 
-    function ethToToken() public payable returns (uint256) {
+    /// @notice Exchanges eth represented in the msg.value sent to the function to token.
+    /// @dev The amount of tokens returned is calculated by the price function.
+    /// @return The amount of tokens that have been bought. 
+    function ethToToken() public payable initialized returns (uint256) {
         uint256 tokenReserve = token.balanceOf(address(this));
-        //ETH is X, tokens are Y
-        //y - b = y'
-        //how many tokens are we getting?
-        uint256 tokensBought = price(msg.value, address(this).balance.sub(msg.value), tokenReserve); // a , x=x'- a, y
+        uint256 tokensBought = price(msg.value, address(this).balance.sub(msg.value), tokenReserve); 
         require(token.transfer(msg.sender, tokensBought), "failed to transfer ETH");
+        emit EthToTokenExchanged(msg.sender, msg.value, tokensBought);
         return tokensBought;
     }
 
-    function tokenToEth(uint256 tokens) public payable returns (uint256) {
+    /// @notice Exchanges DEX tokens for ETH
+    /// @dev This function will need approval from user to this contract to transfer their DEX tokens.
+    /// @param tokens The amount of DEX tokens the user wants to exchange.
+    /// @return The amount of ETH the user gets in return for exchaning tokens.
+    function tokenToEth(uint256 tokens) public payable initialized returns (uint256) {
+
+   
         uint256 tokenReserve = token.balanceOf(address(this));
-        //in this case, tokens is X, Y is eth
-        //y - b = y'
-        //how many ETH are we getting?
-        uint256 ethBought = price(tokens, tokenReserve, address(this).balance); // a , x, y
+        uint256 ethBought = price(tokens, tokenReserve, address(this).balance); 
         require(token.transferFrom(msg.sender, address(this), tokens), "failed to transfer tokens");
         (bool sent, ) = msg.sender.call{value: ethBought}("");
         require(sent, "failed to send ETH");
+        emit TokenToEthExchanged(msg.sender, tokens, ethBought)
         return ethBought;
     }
+        
+    /// @notice Deposits reserve assets into the pool and gives user in exchange LP tokens, that represent the user's share in the pool.
+    /// @dev The amount of tokens introduced into the pool will depend on how much ETH is sent and the pools asset ratio, the user will need to approve the token amount.
+    /// @return The LP token amount minted to the user.
+    function deposit() public payable initialized returns (uint256) {
 
-    function deposit() public payable returns (uint256) {
-        //checks the original ETH reserve, subtracting what we have sent
         uint256 eth_reserve = address(this).balance - msg.value;
-        //Token reserve
         uint256 token_reserve = token.balanceOf(address(this));
         //token amount example with a pool with reserves of 4 eth and 8000 Dai
         // we send 1 eth, 1 * 8000 / 4 = 2000, therefore it will input the balance of 1 2000, which is correct.
@@ -89,18 +138,19 @@ contract DexV1 {
 
         //((eth sent * total liquidity shares ) / eth reserves ) + 1
         // the previous formula with 18 decimals makes it so that the LP tokens minted to the user is
-        //equal to the eth send, since the total liquidity shares in V1 is always going to be equal to the eth reserves.
+        //equal to the eth sent, since the total liquidity shares in V1 is always going to be equal to the eth reserves.
         uint256 liquidity_minted = (msg.value * totalLiquidity) / eth_reserve;
-        emit deposited(liquidity_minted, msg.value);
-        (bool success, ) = LPTokenAddress.call(
-            abi.encodeWithSignature("mintTokensTo(address,uint256)", msg.sender, liquidity_minted)
-        );
-        require(success, "mint tx failed");
+        
         //liquidity tokens added to user balance
         //update total liquidity for future liquidity operations
         totalLiquidity = totalLiquidity + liquidity_minted;
         //call transferFrom with the approved tokens to this contract to finish adding liquidity
-        require(token.transferFrom(msg.sender, address(this), token_amount));
+        require(token.transferFrom(msg.sender, msg.value, token_amount));
+        (bool success, ) = LPTokenAddress.call(
+            abi.encodeWithSignature("mintTokensTo(address,uint256)", msg.sender, liquidity_minted)
+        );
+        require(success, "mint tx failed");
+        emit Deposited(msg.sender, msg.value, token_amount);
         return liquidity_minted;
 
         //on V2 the process is transferFunction agnostic, there is no approval, instead, the tokens must be sent to the contract,
@@ -108,7 +158,10 @@ contract DexV1 {
         //based on the difference between the balanceOf its own address in the ERC20 contract, with its own balance data structure.
     }
 
-    function withdraw(uint256 amount) public returns (uint256, uint256) {
+    /// @notice Burns LP tokens from the user and transfers the underlying assets the tokens represented from the pool.
+    /// @dev The minting and burning of the LP tokens can only be performed by this contract.
+    /// @return The ETH amount and the DEX token amount returned for withdrawing the liquidity.
+    function withdraw(uint256 amount) public initialized returns (uint256, uint256) {
         //ERC20 token call to know what is the balance of this contract
         uint256 token_reserve = token.balanceOf(address(this));
         //on the same pool mentioned before, with 5 eth and 10000 DAI the user inputs 1 as amount
@@ -117,7 +170,6 @@ contract DexV1 {
         //1 * 10000 / 5 = 2000
         uint256 token_amount = amount.mul(token_reserve) / totalLiquidity;
         //liquidity subtracted from the users liquidity balance -1 = 0
-        emit withdrawed(token_amount, amount);
         //only this contract controls the burning and minting of LP tokens.
         (bool success, ) = LPTokenAddress.call(
             abi.encodeWithSignature("burnTokensTo(address,uint256)", msg.sender, amount)
@@ -128,9 +180,16 @@ contract DexV1 {
         payable(msg.sender).transfer(eth_amount);
         //transfer 2000 dai to user
         require(token.transfer(msg.sender, token_amount));
+        emit withdrawed(msg.sender, eth_amount, token_amount);
         return (eth_amount, token_amount);
     }
-
+   
+    /// @notice Function thats called when performing exchanges that calculates the return amount based on the constant product market making algorithm.
+    /// @dev The same formula is used for both types of exchanges, the inputs are set acordingly in the previous function call.
+    /// @param a The amount of asset x added to the pool.
+    /// @param x The reserve amount of asset x prior to the exchange.
+    /// @param y The reserve amount of asset y prior to the exchange,
+    /// @return The amount of asset y taken out of the pool and transfered to the user for the exchanged asset x.
     function price(uint256 a, uint256 x, uint256 y) private pure returns (uint256) {
         //the constant k remains the same
         //x * y = k
@@ -157,6 +216,10 @@ contract DexV1 {
         return numerator / denominator;
     }
 
+    /// @notice Function thats previews the DEX token amount returned for a ETH amount exchanged, without changing state.
+    /// @dev Used to certify correct calculation off-chain
+    /// @param msgValue The amount of ETH the user would be sending to exchange in a ethToToken call as msg.value
+    /// @return The amount of tokens the user would recieve in return for the ETH amount inputed.
     function ethToTokenView(uint256 msgValue) public view returns (uint256) {
         uint256 tokenReserve = token.balanceOf(address(this));
         //ETH is X, tokens are Y
@@ -167,6 +230,10 @@ contract DexV1 {
         return tokensBought;
     }
 
+    /// @notice Function thats previews the ETH amount returned for DEX token amount exchanged, without changing state.
+    /// @dev Used to certify correct calculation off-chain
+    /// @param tokens The amount of tokens we would be exchanging.
+    /// @return The amount of ETH the user would recieve in return for the DEX token amount inputed.
     function tokenToEthView(uint256 tokens) public view returns (uint256) {
         uint256 tokenReserve = token.balanceOf(address(this));
         //in this case, tokens is X, Y is eth
